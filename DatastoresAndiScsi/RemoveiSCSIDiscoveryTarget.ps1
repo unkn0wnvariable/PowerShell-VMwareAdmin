67625a078a38c89b3b38476b25b4503c692a4d81
Oliver Andrews <unkn0wnvariable@users.noreply.github.com>
2018-09-28 13:24:34 +0100

    New script to remove iSCSI discovery targets from ESXi hosts

---
+++ DatastoresAndiScsi/RemoveiSCSIDiscoveryTarget.ps1
@@ -0,0 +1,42 @@
+# Script to remove an iSCSI target from all ESXi hosts in a vCenter
+#
+# Updated for PowerCLI 10
+#
+
+# What is the address of the target are we removing?
+$addressToRemove = '10.130.8.203'
+
+# Get Credentials
+$viCredential = Get-Credential -Message 'Enter credentials for VMware connection'
+
+# Get vSphere server name
+$viServer = Read-Host -Prompt 'Enter hostname of vSphere server'
+
+# Import the PowerCLI Module and Connect
+Import-Module -Name VMware.PowerCLI -Force
+Connect-VIServer -Server $viServer -Credential $viCredential
+
+# Get all hosts attached to vSphere server
+$vmHosts = Get-VMHost -Server $viServer | Sort-Object -Property Name
+
+# Iterate through the hosts removing the iSCSI target where it is present
+foreach($vmHost in $vmHosts){
+    Write-Host -Object ('Removing iSCSI target ' + $addressToRemove + ' from ' + $vmHost + '... ') -NoNewline
+    $vmHostHba = $vmHost | Get-VMHostHba -Type IScsi
+    $targetToRemove = Get-IScsiHbaTarget -IScsiHba $vmHostHba | Where-Object {$_.Address -eq $addressToRemove}
+    if ($targetToRemove) {
+        try {
+            Remove-IScsiHbaTarget -Target $targetToRemove -Confirm:$false -ErrorAction:Stop
+            Write-Host -Object 'Removed OK.' -ForegroundColor Green
+        }
+        catch {
+            Write-Host -Object 'Failed.' -ForegroundColor Red
+        }
+    }
+    else {
+        Write-Host -Object 'Not configured.' -ForegroundColor Red
+    }
+}
+
+# Disconnect from the vSphere server
+Disconnect-VIServer -Server $viServer -Confirm:$false
